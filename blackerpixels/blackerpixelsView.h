a46ee02efeca45db0a7e508bdd017b010c336bb4
pbaker <me@retrodict.com>
2012-05-02 16:23:34 -0400

    Initial Commit

---
+++ blackerpixels/blackerpixelsView.h
@@ -0,0 +1,13 @@
+//
+//  blackerpixelsView.h
+//  blackerpixels
+//
+//  Created by phill on 5/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface blackerpixelsView : ScreenSaverView
+
+@end
